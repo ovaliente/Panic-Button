fd858cce4db08e149220007ff594a6d4a1067743
Haess <hromaza@googlemail.com>
2012-10-01 00:45:04 -0500

    Se añade estructura de aplicación móvil basada en phonegap

---
+++ phonegap/src/com/example/panicbutton/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.panicbutton;
+
+import android.os.Bundle;
+import android.app.Activity;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+}
