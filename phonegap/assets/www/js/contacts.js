9a8475d966a1d533616dcbef38291f05d942a08d
Haess <hromaza@googlemail.com>
2012-10-23 23:23:57 -0500

    Ya se puede obtener email de contactos, falta implementar el WS

--- phonegap/assets/www/js/contacts.js
+++ phonegap/assets/www/js/contacts.js
@@ -11,12 +11,11 @@ function onDeviceReady() {
 
 function onSuccess(contacts) {
     for (var i=0; i<contacts.length; i++) {
-        	alert('111111111');
-        	var contactmail = contacts[i].emails[0];
+        	var contactmail = contacts[i].emails[0].value;
         	var contactname = contacts[i].displayName;
+        	$('.lista').append('<li onclick="muestra(this.id)" id="'+ contactmail +'" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-last ui-btn-up-c"><div class="ui-btn-inner ui-li"><div class="ui-btn-text"><a class="ui-link-inherit">'+ contactname+'</a></div><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></div></li>');
         	
-        	
-    		$('.lista').append('<li id="'+contactmail+'"><a>' + contactname + '</a></li>');
+    		
     		}
     }
 
@@ -26,3 +25,7 @@ function onSuccess(contacts) {
 function onError(contactError) {
     alert("onError!");
 }
+
+function muestra(str){
+	 	alert(str);
+		}
