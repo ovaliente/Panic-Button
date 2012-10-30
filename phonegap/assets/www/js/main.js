d21430a170a5c9c36a95ea47e5638886b36839ea
Haess <hromaza@googlemail.com>
2012-10-30 18:16:28 -0500

    Cambio de logica para crear DB Local

--- phonegap/assets/www/js/main.js
+++ phonegap/assets/www/js/main.js
@@ -1,22 +1,29 @@
 var db;
 var dbCreated = false;
 var watch_id = null;    // ID of the geolocation
-var tmuestreo=3000; // Array containing GPS position objects
+var tmuestreo=3000; // 
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     db = window.openDatabase("panicdb", "1.0", "PhoneGap Demo", 200);
     //Verifico que DB Local exista o no
-    if (dbCreated)
-    	alert('Ya existe DB');
+    //alert(dbCreated);
+    //db.transaction(verifica, transaction_error);
+    //alert(dbCreated);
+    
+    //if (dbCreated){
+    	//alert(dbCreated);
+    	//alert('Ya existe DB');
         // Seteo la frecuencia de muestreo
     	//db.transaction(getData, transaction_error);
     	//tmuestreo='';
-    else
+   //}
+    //else{
     	//$(".img-swap").hide();
     	//alert('Configura Panic Button antes de usarlo');
-    	db.transaction(creaDB, transaction_error, creaDB_success);
+    db.transaction(creaDB, transaction_error, creaDB_success);
+    //}
 }
 
 function transaction_error(tx, error) {
@@ -24,16 +31,16 @@ function transaction_error(tx, error) {
 }
 
 function creaDB_success() {
-	dbCreated = true;
+	//dbCreated = true;
 	alert('Se ha creado correctamente la DB!');
     //db.transaction(getEmployees, transaction_error);
 }
 
 //Creo DB Local con datos de configuracion por defecto, para luego ser actualizada
 function creaDB(tx){
-	tx.executeSql('DROP TABLE IF EXISTS panicdb');
-	tx.executeSql('CREATE TABLE panicdb (id INTEGER PRIMARY KEY, nombre VARCHAR(50), iduser INTEGER, idruta INTEGER, tmuestra INTEGER)');
-	tx.executeSql('INSERT INTO panicdb VALUES(1, "Haessler",0,0,5)');
+	//tx.executeSql('DROP TABLE IF EXISTS panicdb');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS panicdb (id INTEGER PRIMARY KEY, nombre VARCHAR(50), iduser INTEGER, idruta INTEGER, tmuestra INTEGER)');
+	tx.executeSql('INSERT OR IGNORE INTO panicdb VALUES(1, "Haessler",0,0,5)');
 }
 
 function prueba()
@@ -55,11 +62,24 @@ watch_id = navigator.geolocation.watchPosition(
     },
     
     // Settings maximumAge en lugar de frequency para tomar datos cada cierto tiempo
-    { maximumAge: tmuestreo, enableHighAccuracy: true });
+    { maximumAge: tmuestreo, timeout: 5000, enableHighAccuracy: true }); //Usar frecuency en lugar de maximunAge
 }
 
+function verifica(tx) {
+	var sql = "SELECT * FROM panicdb";
+	tx.executeSql(sql, [], valor, transaction_error);
+}
 
-
+function  valor(tx,results){
+	
+	var registro = results.rows.item(0);
+	//alert(registro.id + 'En caso Exista');
+	if (registro.id==1){
+		//alert('verifico id y seteo dbCreated a True');
+		dbCreated=true;
+	}
+	
+}
 
 
 
